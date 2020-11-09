4a72f46d7d7b63d0dc08afd127390bb7bc761dfa
Ivo Slanina <ivo.slanina@gmail.com>
2020-11-09 16:40:51 +0100

    Added tests

--- src/test/groovy/cz/slanina/ms/controller/MeasurementControllerSpecification.groovy
+++ src/test/groovy/cz/slanina/ms/controller/MeasurementControllerSpecification.groovy
@@ -1,5 +1,6 @@
 package cz.slanina.ms.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import cz.slanina.ms.model.Measurement
 import cz.slanina.ms.repository.MeasurementsRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.hateoas.MediaTypes
 import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
@@ -16,7 +18,10 @@ import java.time.OffsetDateTime
 import java.time.ZoneOffset
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
@@ -27,6 +32,16 @@ class MeasurementControllerSpecification extends Specification {
 
     @Autowired
     MeasurementsRepository repository
+
+    @Autowired
+    ObjectMapper objectMapper
+
+    def "context loads"() {
+        expect:
+        mvc
+        repository
+        objectMapper
+    }
 
     def "get all measurements"() {
         when:
@@ -58,6 +73,87 @@ class MeasurementControllerSpecification extends Specification {
                     timestamp: OffsetDateTime.of(2020, 1, 1, 2, 0, 0, 0, ZoneOffset.UTC)
             )
         ]
+    }
+
+    def "get single measurement"() {
+        when:
+        mvc.perform(get("/measurements/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath('$.id').value(1))
+                .andExpect(jsonPath('$.temperature').value(10.0))
+                .andExpect(jsonPath('$.timestamp').value('2020-01-01T01:00:00Z'))
+                .andExpect(jsonPath('$._links.self.href').value('http://localhost/measurements/1'))
+                .andExpect(jsonPath('$._links.measurements.href').value('http://localhost/measurements'))
+
+        then:
+        repository.findById(1) >> Optional.of(new Measurement(
+                id: 1,
+                temperature: 10.0,
+                timestamp: OffsetDateTime.of(2020, 1, 1, 1, 0, 0, 0, ZoneOffset.UTC)
+        ))
+    }
+
+    def "create new measurement"() {
+        given:
+        def measurement = new Measurement(
+                temperature: 10.0,
+                timestamp: OffsetDateTime.of(2020, 1, 1, 2, 0, 0, 0, ZoneOffset.UTC)
+        )
+
+        when:
+        mvc.perform(post("/measurements")
+                .content(objectMapper.writeValueAsString(measurement))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString()))
+                .andExpect(jsonPath('$.id').value(1))
+                .andExpect(jsonPath('$.temperature').value(10.0))
+                .andExpect(jsonPath('$.timestamp').value('2020-01-01T02:00:00Z'))
+                .andExpect(jsonPath('$._links.self.href').value('http://localhost/measurements/1'))
+
+        then:
+        repository.save(measurement) >> new Measurement(
+                id: 1,
+                temperature: 10.0,
+                timestamp: OffsetDateTime.of(2020, 1, 1, 2, 0, 0, 0, ZoneOffset.UTC)
+        )
+    }
+
+    def "update existing measurement"() {
+        given:
+        def measurement = new Measurement(
+                temperature: 20.0,
+                timestamp: OffsetDateTime.of(2020, 1, 1, 2, 0, 0, 0, ZoneOffset.UTC)
+        )
+
+        when:
+        mvc.perform(put("/measurements/1")
+                .content(objectMapper.writeValueAsString(measurement))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent())
+
+        then:
+        repository.save(new Measurement(
+                id: 1,
+                temperature: 20.0,
+                timestamp: OffsetDateTime.of(2020, 1, 1, 2, 0, 0, 0, ZoneOffset.UTC)
+        ))
+    }
+
+    def "delete measurement"() {
+        when:
+        mvc.perform(delete("/measurements/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent())
+
+        then:
+        repository.deleteById(1)
     }
 
     @SuppressWarnings("unused")
