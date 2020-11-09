8b1b5b74a13a815a2c0d0e1179f6dbc0bf1016a1
Ivo Slanina <ivo.slanina@gmail.com>
2020-11-09 14:28:24 +0100

    Added tests

--- src/test/groovy/cz/slanina/ms/controller/MeasurementControllerSpecification.groovy
+++ src/test/groovy/cz/slanina/ms/controller/MeasurementControllerSpecification.groovy
@@ -1,16 +1,23 @@
 package cz.slanina.ms.controller
 
+import cz.slanina.ms.model.Measurement
 import cz.slanina.ms.repository.MeasurementsRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.hateoas.MediaTypes
+import org.springframework.http.HttpHeaders
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
 class MeasurementControllerSpecification extends Specification {
@@ -22,9 +29,35 @@ class MeasurementControllerSpecification extends Specification {
     MeasurementsRepository repository
 
     def "get all measurements"() {
-        expect:
-        mvc.perform(MockMvcRequestBuilders.get("/measurements"))
+        when:
+        mvc.perform(get("/measurements"))
+                .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath('$._embedded.measurements[0].id').value(1))
+                .andExpect(jsonPath('$._embedded.measurements[0].temperature').value(10.0))
+                .andExpect(jsonPath('$._embedded.measurements[0].timestamp').value('2020-01-01T01:00:00Z'))
+                .andExpect(jsonPath('$._embedded.measurements[0]._links.self.href').value('http://localhost/measurements/1'))
+                .andExpect(jsonPath('$._embedded.measurements[0]._links.measurements.href').value('http://localhost/measurements'))
+                .andExpect(jsonPath('$._embedded.measurements[1].id').value(2))
+                .andExpect(jsonPath('$._embedded.measurements[1].temperature').value(11.0))
+                .andExpect(jsonPath('$._embedded.measurements[1].timestamp').value('2020-01-01T02:00:00Z'))
+                .andExpect(jsonPath('$._embedded.measurements[1]._links.self.href').value('http://localhost/measurements/2'))
+                .andExpect(jsonPath('$._embedded.measurements[1]._links.measurements.href').value('http://localhost/measurements'))
+
+        then:
+        repository.findAll() >> [
+            new Measurement(
+                    id: 1,
+                    temperature: 10.0,
+                    timestamp: OffsetDateTime.of(2020, 1, 1, 1, 0, 0, 0, ZoneOffset.UTC)
+            ),
+            new Measurement(
+                    id: 2,
+                    temperature: 11.0,
+                    timestamp: OffsetDateTime.of(2020, 1, 1, 2, 0, 0, 0, ZoneOffset.UTC)
+            )
+        ]
     }
 
     @SuppressWarnings("unused")
